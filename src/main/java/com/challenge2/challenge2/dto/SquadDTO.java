2e3f979f1982484dd2313a87325c246f9782fb45
diasvictordev <victor.dias@aluno.ueg.br>
2023-08-22 10:32:43 -0300

    modificando squad para record

--- src/main/java/com/challenge2/challenge2/dto/SquadDTO.java
+++ src/main/java/com/challenge2/challenge2/dto/SquadDTO.java
@@ -1,18 +1,7 @@
 package com.challenge2.challenge2.dto;
 
-import jakarta.persistence.Column;
-import lombok.*;
-
+import lombok.Builder;
 import java.util.List;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class SquadDTO {
-    private String squadName;
-    private List<Long> students;
-
-}
+public record SquadDTO (String squadName, List<Long> students){}
